f4b7ee1d912fc6948278814fc8878245e414c153
ADNAN_DEV <mh.shamey@gmail.com>
2024-08-06 18:00:01 +0300

    fix users Table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\LevelUserEnum;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,7 +17,9 @@ class DatabaseSeeder extends Seeder
 
          \App\Models\User::factory()->create([
              'name' => 'Test User',
-             'email' => 'test@example.com',
+             'email' => 'admin@admin.com',
+             'password'=>bcrypt('password'),
+             'level'=>LevelUserEnum::ADMIN->value
          ]);
     }
 }
