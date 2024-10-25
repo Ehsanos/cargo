cf93bf31beefa0a7d7b1718c8ca79f4f729313fe
ADNAN_DEV <mh.shamey@gmail.com>
2024-10-25 20:19:18 +0300

    fix login redirect to branch

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable implements HasMedia, FilamentUser, HasAvatar
     use HasApiTokens, HasFactory, Notifiable, HasRoles, InteractsWithMedia;
     use HasPanelShield;
 
-
+/*
    public function canAccessPanel(Panel $panel): bool
     {
         if ($panel->getId() === 'admin' && $this->level == LevelUserEnum::ADMIN) {
@@ -45,7 +45,7 @@ class User extends Authenticatable implements HasMedia, FilamentUser, HasAvatar
         }
         return false;
 
-    }
+    }*/
 
     public function getFilamentAvatarUrl(): ?string
     {
