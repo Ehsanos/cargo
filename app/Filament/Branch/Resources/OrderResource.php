58c24453cb43dc8ee8c79ba6118ecfa8d92007bb
Ehsano Gamor <ehsan.gamor90@gmail.com>
2024-10-25 19:49:41 +0300

    phone empty

--- app/Filament/Branch/Resources/OrderResource.php
+++ app/Filament/Branch/Resources/OrderResource.php
@@ -232,7 +232,8 @@ class OrderResource extends Resource
                 Tables\Columns\TextColumn::make('bay_type')->label('حالة الدفع'),
                 Tables\Columns\TextColumn::make('sender.name')->label('اسم المرسل'),
                 Tables\Columns\TextColumn::make('sender.phone')->label('هاتف المرسل')
-                    ->url(fn($record) => url('https://wa.me/' . ltrim($record->receive->phone, '+')))->openUrlInNewTab()
+                    ->url(fn($record) => url('https://wa.me/' . ltrim($record->receive?->phone, '+')))
+                    ->openUrlInNewTab()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('citySource.name')->label('من بلدة'),
                 Tables\Columns\TextColumn::make('receive.name')->label('معرف المستلم '),
@@ -240,7 +241,8 @@ class OrderResource extends Resource
 
                 Tables\Columns\TextColumn::make('receive.address')->label('عنوان المستلم ')->searchable(),
                 Tables\Columns\TextColumn::make('receive.phone')->label('هاتف المستلم ')
-                    ->url(fn($record) => url('https://wa.me/' . ltrim($record->receive->phone, '+')))->openUrlInNewTab()
+                    ->url(fn($record) => url('https://wa.me/' . ltrim($record->receive?->phone, '+')))
+                    ->openUrlInNewTab()
                     ->searchable(),
 
                 Tables\Columns\TextColumn::make('cityTarget.name')->label('الى مدينة '),
