811f0b4b59f1a6f655f475702a892a1d5b1a7a15
Ehsano Gamor <ehsan.gamor90@gmail.com>
2024-10-25 22:49:28 +0300

    branch resources

--- app/Filament/Branch/Resources/OrderResource.php
+++ app/Filament/Branch/Resources/OrderResource.php
@@ -145,6 +145,7 @@ class OrderResource extends Resource
                                  Forms\Components\TextInput::make('global_name')->label('اسم المستلم'),
 
                                  Forms\Components\TextInput::make('receive_address')->label('عنوان المستلم')->required(),
+                                 Forms\Components\TextInput::make('receive_phone')->label('هاتف المستلم'),
                                  Forms\Components\Select::make('city_target_id')
                                      ->relationship('cityTarget', 'name')
                                      ->label('الى مدينة')->required()->searchable()->preload()
