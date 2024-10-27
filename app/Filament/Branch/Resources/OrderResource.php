ec6a14a586a8b5c0e744489d6f143da59d450a98
Ehsano Gamor <ehsan.gamor90@gmail.com>
2024-10-27 11:52:15 +0300

    adding live set for city + branches

--- app/Filament/Branch/Resources/OrderResource.php
+++ app/Filament/Branch/Resources/OrderResource.php
@@ -80,20 +80,7 @@ class OrderResource extends Resource
                             Forms\Components\Select::make('city_source_id')
                                 ->relationship('citySource', 'name')
                                 ->label('من بلدة')->reactive()->required()->searchable()->preload()
-                                ->afterStateUpdated(function ($state, $set) {
-
-                                    if ($state != null) {
-
-                                        $city = City::find($state);
-                                        $set('branch_source_id', $city?->branch_id);
-
-                                    } else {
-
-                                        $set('branch_source_id', null);
-
-                                    }
-                                })
-                                ->live(),
+                            ,
 
                             /*
 
