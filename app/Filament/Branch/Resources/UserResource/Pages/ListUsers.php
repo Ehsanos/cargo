c8edb702c5f1d26e8910f2c16a3f48961543f68d
Ehsano Gamor <ehsan.gamor90@gmail.com>
2024-10-27 08:17:17 +0300

    add user filter to branch resources

--- app/Filament/Branch/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Branch/Resources/UserResource/Pages/ListUsers.php
@@ -29,8 +29,8 @@ class ListUsers extends ListRecords
     {
         return [
             Tab::make('staff')->modifyQueryUsing(fn($query)=>$query->where('level',LevelUserEnum::STAFF->value))->label('الموظفين'),
-            /*Tab::make('branch')->modifyQueryUsing(fn($query)=>$query->where('level',LevelUserEnum::BRANCH->value))->label('مدراء الأفرع'),*/
-          /*  Tab::make('admin')->modifyQueryUsing(fn($query)=>$query->where('level',LevelUserEnum::ADMIN->value))->label('المدراء'),*/
+            Tab::make('branch')->modifyQueryUsing(fn($query)=>$query->where('level',LevelUserEnum::BRANCH->value))->label('مدراء الأفرع'),
+           Tab::make('admin')->modifyQueryUsing(fn($query)=>$query->where('level',LevelUserEnum::ADMIN->value))->label('المدراء'),
             Tab::make('user')->modifyQueryUsing(fn($query)=>$query->where('level',LevelUserEnum::USER->value))->label('المستخدمين'),
             Tab::make('user')->modifyQueryUsing(fn($query)=>$query->where('id','!=',''))->label('الكل'),
         ];
