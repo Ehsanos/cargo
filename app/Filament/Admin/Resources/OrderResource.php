b9da3f2419112ae144e0ec404cc9474d4ea207a4
adnan <mh.shamey@gmail.com>
2024-11-04 14:34:24 +0300

    add balance policy

--- app/Filament/Admin/Resources/OrderResource.php
+++ app/Filament/Admin/Resources/OrderResource.php
@@ -273,7 +273,7 @@ class OrderResource extends Resource
 
                         ]),
                         Forms\Components\Grid::make()->schema([
-                            Forms\Components\Select::make('pick_id')->label('الموظف الملتقط')->options(User::where('level',LevelUserEnum::BRANCH->value)->orWhere('level',LevelUserEnum::STAFF->value)->pluck('name','id'))->searchable(),
+                            Forms\Components\Select::make('pick_id')->label('الموظف الملتقط')->options(User::where('level',LevelUserEnum::BRANCH->value)->orWhere('level',LevelUserEnum::STAFF->value)->pluck('name','id'))->searchable()->required(),
 
                         ]),
 
