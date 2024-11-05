215962a2bb2b929d6cd969faf730ab2d0a1f505c
adnan <mh.shamey@gmail.com>
2024-11-05 11:03:53 +0300

    add phone to tasks

--- app/Filament/Branch/Widgets/TaskWidget.php
+++ app/Filament/Branch/Widgets/TaskWidget.php
@@ -20,7 +20,10 @@ class TaskWidget extends BaseWidget
             ->columns([
                 Tables\Columns\TextColumn::make('id')->label('التسلسل'),
                 Tables\Columns\TextColumn::make('from')->label('إستلام من'),
+                Tables\Columns\TextColumn::make('sender_phone')->label('هاتف المرسل')->url(fn($state)=>"https://wa.me/".trim($state,'+'),true),
                 Tables\Columns\TextColumn::make('to')->label('التسليم لـ'),
+                Tables\Columns\TextColumn::make('receive_phone')->label('هاتف المستلم')->url(fn($state)=>"https://wa.me/".trim($state,'+'),true),
+
                 Tables\Columns\TextColumn::make('task')->label('المهمة'),
                 Tables\Columns\TextColumn::make('created_at')->since()->label('منذ')->sortable(),
 
--- app/Filament/Employ/Widgets/TaskWidget.php
+++ app/Filament/Employ/Widgets/TaskWidget.php
@@ -9,7 +9,8 @@ use Filament\Widgets\TableWidget as BaseWidget;
 
 class TaskWidget extends BaseWidget
 {
-    protected static ?string $heading="المهام الإدارية";
+    protected static ?string $heading = "المهام الإدارية";
+
     public function table(Table $table): Table
     {
         return $table
@@ -20,11 +21,15 @@ class TaskWidget extends BaseWidget
             ->columns([
                 Tables\Columns\TextColumn::make('id')->label('التسلسل'),
                 Tables\Columns\TextColumn::make('from')->label('إستلام من'),
+                Tables\Columns\TextColumn::make('sender_phone')->label('هاتف المرسل')->url(fn($state) => "https://wa.me/" . trim($state, '+'), true),
+
                 Tables\Columns\TextColumn::make('to')->label('التسليم لـ'),
+                Tables\Columns\TextColumn::make('receive_phone')->label('هاتف المستلم')->url(fn($state) => "https://wa.me/" . trim($state, '+'), true),
+
                 Tables\Columns\TextColumn::make('task')->label('المهمة'),
                 Tables\Columns\TextColumn::make('created_at')->since()->label('منذ')->sortable(),
             ])->actions([
-                Tables\Actions\Action::make('complete')->label('إتمام')->requiresConfirmation()->action(fn($record) => $record->update(['is_complete'=> true]))
+                Tables\Actions\Action::make('complete')->label('إتمام')->requiresConfirmation()->action(fn($record) => $record->update(['is_complete' => true]))
             ]);
     }
 }
