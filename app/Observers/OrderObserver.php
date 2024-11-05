699b035c37923b2e22baa9001a26291a8ee199fd
adnan <mh.shamey@gmail.com>
2024-11-05 13:37:51 +0300

    checked tasks

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -26,22 +26,6 @@ class OrderObserver
      */
     public function created(Order $order): void
     {
-        info('PICK:'.$order->pick_id);
-        if( $order->pick_id!=null){
-            \DB::beginTransaction();
-            try{
-                HelperBalance::completePicker($order);
-                $order->status=OrderStatusEnum::PICK;
-                $order->save();
-                info('complete success order');
-                \DB::commit();
-            }catch (\Exception |\Error $e){
-                \DB::rollBack();
-                info("Error Observe");
-                info('Message:'.$e->getMessage());
-            }
-
-        }
 
     }
 
