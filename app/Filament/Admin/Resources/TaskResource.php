5378ba3575233ce44c6d6cb7987d41dc20ecc834
adnan <mh.shamey@gmail.com>
2024-11-05 11:29:56 +0300

    add phone to tasks

--- app/Filament/Admin/Resources/TaskResource.php
+++ app/Filament/Admin/Resources/TaskResource.php
@@ -9,6 +9,7 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -61,7 +62,8 @@ class TaskResource extends Resource
 
             ])
             ->filters([
-                Tables\Filters\SelectFilter::make('user_id')->relationship('user', 'name')->label('المستخدم')->searchable()
+                Tables\Filters\SelectFilter::make('user_id')->relationship('user', 'name')->label('المستخدم')->searchable(),
+                TernaryFilter::make('is_complete')->lable('حالة المهمة'),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
