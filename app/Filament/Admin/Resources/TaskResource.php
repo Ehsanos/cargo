40d53bbac90220361f54992369b304aa73825647
adnan <mh.shamey@gmail.com>
2024-11-05 10:53:52 +0300

    ف

--- app/Filament/Admin/Resources/TaskResource.php
+++ app/Filament/Admin/Resources/TaskResource.php
@@ -40,6 +40,7 @@ class TaskResource extends Resource
         return $table
             ->poll(10)
             ->columns([
+                Tables\Columns\TextColumn::make('id')->label('التسلسل'),
                 Tables\Columns\TextColumn::make('user.name')->label('المستخدم')->searchable(),
                 Tables\Columns\TextColumn::make('from')->label('إستلام من'),
                 Tables\Columns\TextColumn::make('to')->label('التسليم لـ'),
