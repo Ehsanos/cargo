e8bf18fd75182a7e9d5d283530cd9e69ae765a2d
adnan <mh.shamey@gmail.com>
2024-11-05 13:01:19 +0300

    checked tasks

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -26,6 +26,7 @@ class OrderObserver
      */
     public function created(Order $order): void
     {
+        info('PICK:'.$order->pick_id);
         if( $order->pick_id!=null){
             \DB::beginTransaction();
             try{
