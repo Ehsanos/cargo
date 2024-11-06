820bfd1cfb66080bad9c029b6697e2323c5af718
adnan <mh.shamey@gmail.com>
2024-11-06 14:57:56 +0300

    add currency table

--- app/Filament/Admin/Resources/AccountBalanceResource/Pages/ListAccountBalances.php
+++ app/Filament/Admin/Resources/AccountBalanceResource/Pages/ListAccountBalances.php
@@ -6,6 +6,7 @@ use App\Enums\BalanceTypeEnum;
 use App\Filament\Admin\Resources\AccountBalanceResource;
 use App\Models\Balance;
 use App\Models\User;
+use Closure;
 use Filament\Actions;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Select;
@@ -24,42 +25,58 @@ class ListAccountBalances extends ListRecords
             Actions\CreateAction::make(),
             Actions\Action::make('created')->form([
                 Grid::make(3)->schema([
-                    Select::make('from_user')->options(User::accounts()->pluck('name', "id"))->required()->label('من حساب')->searchable(),
-                    Select::make('to_user')->options(User::accounts()->pluck('name', "id"))->required()->label('إلى حساب')->searchable(),
-                    TextInput::make('value')->numeric()->gt(0)->required()->label('المبلغ'),
+                    Select::make('from')->options(User::accounts()->pluck('name', "id"))->required()->label('الحساب الرئيسي')->searchable(),
+                    Select::make('to')->options(User::accounts()->pluck('name', "id"))->required()->label('الحساب المقابل')->searchable(),
+                    TextInput::make('value')->numeric()->rules([
+                        fn (): Closure => function (string $attribute, $value, Closure $fail) {
+                            if ($value <=0) {
+                                $fail('يجب ان تكون القيمة أكبر من 0');
+                            }
+                        },
+                    ])->required()->label('القيمة بعملة الحساب الرئيسي'),
 
                 ]),
                 Textarea::make('info')->label('البيان')
             ])->action(function ($data) {
+                $accountSource = User::accounts()->find($data['from']);
+
+                $accountTarget = User::accounts()->find($data['to']);
                 \DB::beginTransaction();
-                try {
-                    $userFrom = User::accounts()->where('id', $data['from_user'])->first();
-                    if (!$userFrom /*|| ($userFrom->total_balance < $data['value'])*/) {
-                        throw  new \Exception("الحساب : غير موجود");
+                try{
+                    if($accountSource==null || $accountTarget==null){
+                        throw new \Exception('تأكد من تحديد الحسابات بشكل صحيح');
                     }
+                    $currencySource=$accountSource->currency;
+                    $currencyTarget=$accountTarget->currency;
+                    $value=$data['value']/$currencySource->value;
                     Balance::create([
-                        'credit' => 0,
-                        'debit' => $data['value'],
-                        'user_id' => $data['from_user'],
-                        'info' => $data['info'],
-                        'type' => BalanceTypeEnum::PUSH->value,
+                        'credit'=>0,
+                        'debit'=>$data['value'],
                         'is_complete'=>true,
+                        'pending'=>false,
+                        'user_id'=>$accountSource->id,
+                        'currency_id'=>$accountSource->currency_id,
+                        'ex_cur'=>$accountSource->currency?->value,
+                        'info'=>'تحويل إلى حساب #'.$accountTarget->name.' - '. $data['info'],
                     ]);
                     Balance::create([
-                        'credit' => $data['value'],
-                        'debit' => 0,
-                        'user_id' => $data['to_user'],
-                        'info' => $data['info'],
-                        'type' => BalanceTypeEnum::CATCH->value,
+                        'credit'=>$value*$currencyTarget->value,
+                        'debit'=>0,
                         'is_complete'=>true,
+                        'pending'=>false,
+                        'user_id'=>$accountTarget->id,
+                        'currency_id'=>$accountTarget->currency_id,
+                        'ex_cur'=>$accountTarget->currency?->value,
+                        'info'=>'تحويل من حساب #'.$accountSource->name.' - '. $data['info'],
                     ]);
                     \DB::commit();
-                } catch (\Exception | \Error $e) {
+                    Notification::make('success')->success()->title('نجاح العملية')->body('تم التحويل')->send();
+                }catch (\Exception | \Error $e){
                     \DB::rollBack();
-                    Notification::make('error')->title('فشل العملية')->body($e->getMessage())->danger()->send();
+                    Notification::make('error')->danger()->title('فشل العملية')->body($e->getMessage())->send();
                 }
 
-            })
+            })->label('إضافة سند')
         ];
     }
 }
