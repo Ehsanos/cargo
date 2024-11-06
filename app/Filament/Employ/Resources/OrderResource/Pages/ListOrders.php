255ad92231cab0b7138f0898f2ae84ea41c89e27
adnan <mh.shamey@gmail.com>
2024-11-06 16:32:15 +0300

    add currency table

--- app/Filament/Employ/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Employ/Resources/OrderResource/Pages/ListOrders.php
@@ -26,17 +26,16 @@ class ListOrders extends ListRecords
     public function getTabs(): array
     {
         return [
-            Tab::make('pending')->modifyQueryUsing(fn($query)=>$query->where('status',
-                OrderStatusEnum::PENDING->value))->badge(Order::where('status',OrderStatusEnum::PENDING->value)
-                ->count())->label('بإنتظار الموافقة'),
-            Tab::make('agree')->modifyQueryUsing(fn($query)=>$query->where('status',OrderStatusEnum::AGREE->value))->badge(Order::where('status',OrderStatusEnum::AGREE->value)->count())->label('بإنتظار الإنهاء'),
+            Tab::make('all')->modifyQueryUsing(fn($query)=>$query->where('status','!=' ,""))->badge(Order::all()->count())
+                /*->badge(Order::where('status','success')->count())*/->label('الكل'),
+//            Tab::make('pending')->modifyQueryUsing(fn($query)=>$query->where('status',OrderStatusEnum::PENDING->value))->badge(Order::where('status',OrderStatusEnum::PENDING->value)->count())->label('بإنتظار الموافقة'),
+//            Tab::make('agree')->modifyQueryUsing(fn($query)=>$query->where('status',OrderStatusEnum::AGREE->value))->badge(Order::where('status',OrderStatusEnum::AGREE->value)->count())->label('بإنتظار الإنهاء'),
             Tab::make('pick')->modifyQueryUsing(fn($query)=>$query->where('status',OrderStatusEnum::PICK->value))->badge(Order::where('status',OrderStatusEnum::PICK->value)->count())->label('تم الإلتقاط'),
+            Tab::make('transfer')->modifyQueryUsing(fn($query)=>$query->where('status',OrderStatusEnum::TRANSFER->value))->badge(Order::where('status',OrderStatusEnum::TRANSFER->value)->count())->label('بإنتظار التسليم'),
             Tab::make('success')->modifyQueryUsing(fn($query)=>$query->where('status',OrderStatusEnum::SUCCESS->value))/*->badge(Order::where('status','success')->count())*/->label('منتهي'),
             Tab::make('canceled')->modifyQueryUsing(fn($query)=>$query->where('status',OrderStatusEnum::CANCELED->value))/*->badge(Order::where('status','success')->count())*/->label('ملغي'),
             Tab::make('returned')->modifyQueryUsing(fn($query)=>$query->where('status',OrderStatusEnum::RETURNED->value))/*->badge(Order::where('status','success')->count())*/->label('مرتجع'),
-            Tab::make('all')->modifyQueryUsing(fn($query)=>$query->where('status','!=' ,""))->badge(Order::all()
-                ->count())
-                /*->badge(Order::where('status','success')->count())*/->label('الكل'),
+
         ];
     }
 
