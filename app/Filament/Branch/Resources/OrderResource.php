6ccae80fe6e028e97114a2a3c27eda8117c1f909
adnan <mh.shamey@gmail.com>
2024-11-09 17:57:17 +0300

    add Exchange

--- app/Filament/Branch/Resources/OrderResource.php
+++ app/Filament/Branch/Resources/OrderResource.php
@@ -402,7 +402,7 @@ class OrderResource extends Resource
 //                Tables\Columns\TextColumn::make('cityTarget.name')->label('الى بلدة ')->searchable(),
 //                Tables\Columns\TextColumn::make('created_at')->label('تاريخ الشحنة')
 //                    ->formatStateUsing(fn($state) => Carbon::parse($state)->diffForHumans()) // عرض الزمن بشكل نسبي
-                Tables\Columns\TextColumn::make('pick.name')->formatStateUsing(fn($record)=>'موظف الإلتقاط : '.$record->pick?->name)->description(fn($record)=>'موظف التسليم : '.$record->given?->name)
+                Tables\Columns\TextColumn::make('pick.name')->formatStateUsing(fn($record)=>'موظف الإلتقاط : '.$record->pick?->name)->description(fn($record)=>'موظف التسليم : '.$record->given?->name)->label('التوكيل')
 
 
             ])->defaultSort('created_at', 'desc')
