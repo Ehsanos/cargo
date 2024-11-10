07eb2564a203f80fc15dbf2b9932760bb2dc7d33
adnan <mh.shamey@gmail.com>
2024-11-10 12:32:42 +0300

    add Exchange

--- app/Filament/Branch/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Branch/Resources/OrderResource/Pages/ListOrders.php
@@ -77,10 +77,10 @@ class ListOrders extends ListRecords
                         ->orWhere('branch_target_id', auth()->user()->branch_id)
                         ->orWhere('pick_id', auth()->id())
                         ->orWhere('given_id', auth()->id())) ->label('ملغي'),
-                    Tab::make('returned')->modifyQueryUsing(fn($query) => $query->where('status', OrderStatusEnum::RETURNED->value))->where('branch_source_id', auth()->user()->branch_id)
+                    Tab::make('returned')->modifyQueryUsing(fn($query) => $query->where('status', OrderStatusEnum::RETURNED->value)->where('branch_source_id', auth()->user()->branch_id)
                         ->orWhere('branch_target_id', auth()->user()->branch_id)
                         ->orWhere('pick_id', auth()->id())
-                        ->orWhere('given_id', auth()->id()) ->label('مرتجع'),
+                        ->orWhere('given_id', auth()->id())) ->label('مرتجع'),
         ];
     }
 
