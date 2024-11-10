0dcb271215bec4ab9431f4cef16e955107a95e8a
adnan <mh.shamey@gmail.com>
2024-11-10 16:32:57 +0300

    add Exchange

--- app/Filament/Branch/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Branch/Resources/OrderResource/Pages/ListOrders.php
@@ -30,7 +30,12 @@ class ListOrders extends ListRecords
                 'given_id'=>auth()->id(),
                 'branch_source_id'=>auth()->user()->branch_id,
                 'branch_target_id'=>auth()->user()->branch_id,
-            ])))->badge(Order::all()->count())->label('الكل'),
+            ])))->badge(Order::where(fn($query)=>$query->orWhere([
+                'pick_id'=>auth()->id(),
+                'given_id'=>auth()->id(),
+                'branch_source_id'=>auth()->user()->branch_id,
+                'branch_target_id'=>auth()->user()->branch_id,
+            ]))->count())->label('الكل'),
             Tab::make('pick')->modifyQueryUsing(fn($query)=>$query
                 ->where('status',OrderStatusEnum::PICK->value)
                 ->where(fn($query)=>$query->orWhere([
