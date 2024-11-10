0a86859a34e5395f9bda3093958cb489d5359d19
adnan <mh.shamey@gmail.com>
2024-11-10 12:47:11 +0300

    add Exchange

--- app/Filament/Branch/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Branch/Resources/OrderResource/Pages/ListOrders.php
@@ -53,11 +53,7 @@ class ListOrders extends ListRecords
                 ->label('بإنتظار التسليم'),
 
             Tab::make('success')->modifyQueryUsing(fn($query) =>
-            $query->where('status', OrderStatusEnum::SUCCESS->value)
-                ->where(fn($query)=>$query ->where('branch_source_id', auth()->user()->branch_id)
-                    ->orWhere('branch_target_id', auth()->user()->branch_id)
-                    ->orWhere('pick_id', auth()->id())
-                    ->orWhere('given_id', auth()->id())))->label('منتهي'),
+            $query->where('status', OrderStatusEnum::SUCCESS->value))->label('منتهي'),
             Tab::make('canceled')->modifyQueryUsing(fn($query) =>
             $query->where('status', OrderStatusEnum::CANCELED->value))->label('ملغي'),
             Tab::make('returned')->modifyQueryUsing(fn($query) =>
