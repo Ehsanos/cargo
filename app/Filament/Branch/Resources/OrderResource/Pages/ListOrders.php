e3121009cf3528871800b6f616f0c636602d4904
adnan <mh.shamey@gmail.com>
2024-11-10 12:55:51 +0300

    add Exchange

--- app/Filament/Branch/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Branch/Resources/OrderResource/Pages/ListOrders.php
@@ -6,6 +6,7 @@ use App\Enums\OrderStatusEnum;
 use App\Filament\Branch\Resources\OrderResource;
 use App\Models\Order;
 use Filament\Actions;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Components\Tab;
 use Illuminate\Database\Eloquent\Builder;
@@ -13,7 +14,7 @@ use Illuminate\Database\Eloquent\Builder;
 class ListOrders extends ListRecords
 {
     protected static string $resource = OrderResource::class;
-
+    use ExposesTableToWidgets;
     protected function getHeaderActions(): array
     {
         return [
