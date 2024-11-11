484854fba8f3ab8af76505850a9a45619394e526
adnan <mh.shamey@gmail.com>
2024-11-11 12:08:36 +0300

    fix query builder

--- database/migrations/2024_11_11_084409_add_uuid_balances_table.php
+++ database/migrations/2024_11_11_084409_add_uuid_balances_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('balances', function (Blueprint $table) {
-            $table->string('uuid')->nullable()->unique();
+            $table->string('uuid')->nullable();
         });
     }
 
